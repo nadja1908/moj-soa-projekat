ca604cc862927368198f9e621dd2c31b1d3094fa
Sara Svrkota <sara.sara4602@gmail.com>
2025-11-16 10:58:42 +0100

    port

--- services/api-gateway/cmd/main/main.go
+++ services/api-gateway/cmd/main/main.go
@@ -37,7 +37,7 @@ func main() {
 
 	// Configure CORS
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000", "http://127.0.0.1:3000"}
+	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
 	config.AllowCredentials = true
